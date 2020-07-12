7ba6b75da76202abbf1e768d5a8dbbe018e634c6
Kristen Sullivan <you@example.com>
2020-07-12 16:52:07 -0400

    added more assets

--- src/helpers/projectData.js
+++ src/helpers/projectData.js
@@ -39,6 +39,10 @@ export const projectData = [
       jp2:
         "https://s3.us-east-2.amazonaws.com/ks-portfoli.com/images/concora_screenshot_kohler.jp2"
     },
+    slides: [
+      "https://s3.us-east-2.amazonaws.com/ks-portfoli.com/images/concora_ds/concora_ds_1.jpg",
+      "https://s3.us-east-2.amazonaws.com/ks-portfoli.com/images/concora_ds/concora_ds_2.jpg"
+    ],
     responsibilities: [
       "Javascript",
       "React/Redux",
@@ -48,7 +52,8 @@ export const projectData = [
       "Unit, Integration, and E2E Testing",
       "Github"
     ],
-    summary: ""
+    summary:
+      "Product catalog SAAS for the AEC industry that makes it easier for manufacturers to provide product assets and tools to engineers and designers.  Captures actionable user data to enable sales and marketing effectiveness."
   },
   {
     id: "concora-design-manager",
@@ -69,8 +74,12 @@ export const projectData = [
       "Unit, Integration, and E2E Testing",
       "Github"
     ],
+    slides: [
+      "https://s3.us-east-2.amazonaws.com/ks-portfoli.com/images/concora_dm/concora_dm_1.jpg",
+      "https://s3.us-east-2.amazonaws.com/ks-portfoli.com/images/concora_dm/concora_dm_2.jpg"
+    ],
     summary:
-      "Side project with former colleagues, which is in the pre-alpha stage"
+      "Digital content and asset management for AEC manufacturers to control assets and product specs delivered to customer facing design studio interface.  Contains customer data and analytics captured from the design studio interface."
   },
   {
     id: "hirez-smite",
@@ -82,6 +91,12 @@ export const projectData = [
       jp2:
         "https://s3.us-east-2.amazonaws.com/ks-portfoli.com/images/smitegame_screenshot.jp2"
     },
+    slides: [
+      "https://s3.us-east-2.amazonaws.com/ks-portfoli.com/images/smitegame/smitegame_1.jpg",
+      "https://s3.us-east-2.amazonaws.com/ks-portfoli.com/images/smitegame/smitegame_2.jpg",
+      "https://s3.us-east-2.amazonaws.com/ks-portfoli.com/images/smitegame/smitegame_3.jpg",
+      "https://s3.us-east-2.amazonaws.com/ks-portfoli.com/images/smitegame/smitegame_4.jpg"
+    ],
     responsibilities: [
       "Javascript",
       "React/Redux",
@@ -90,10 +105,11 @@ export const projectData = [
       "Wordpress",
       "PHP",
       "Github",
-      "Gatsby"
+      "Gatsby",
+      "Localization"
     ],
     summary:
-      "Side project with former colleagues, which is in the pre-alpha stage"
+      "Website for Hi-Rez Studio's Smite video game, providing news updates as well as in game character and equipment details"
   },
   {
     id: "hirez-paladins",
@@ -105,6 +121,9 @@ export const projectData = [
       jp2:
         "https://s3.us-east-2.amazonaws.com/ks-portfoli.com/images/paladins_home.jp2"
     },
+    slides: [
+      "https://s3.us-east-2.amazonaws.com/ks-portfoli.com/images/paladins/paladins_1.gif"
+    ],
     responsibilities: [
       "Javascript",
       "React/Redux",
@@ -113,10 +132,11 @@ export const projectData = [
       "Wordpress",
       "PHP",
       "Github",
-      "Gatsby"
+      "Gatsby",
+      "Localization"
     ],
     summary:
-      "Side project with former colleagues, which is in the pre-alpha stage"
+      "Website providing updated information (news, champion details, etc.) for Paladins video game."
   },
   {
     id: "hirez-realm-royale",
@@ -128,6 +148,9 @@ export const projectData = [
       jp2:
         "https://s3.us-east-2.amazonaws.com/ks-portfoli.com/images/realmroyale_home.jp2"
     },
+    slides: [
+      "https://s3.us-east-2.amazonaws.com/ks-portfoli.com/images/realmroyale/realmroyale_1.jpg"
+    ],
     responsibilities: [
       "Javascript",
       "React/Redux",
@@ -136,9 +159,9 @@ export const projectData = [
       "Wordpress",
       "PHP",
       "Github",
-      "Gatsby"
+      "Gatsby",
+      "Localization"
     ],
-    summary:
-      "Side project with former colleagues, which is in the pre-alpha stage"
+    summary: "Website providing news updates for Realm Royale video game."
   }
 ];
